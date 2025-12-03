b15c1d50f23790095766590afacea3e609f11ea6
devanujsj11 <devanujsj@gmail.com>
2025-12-03 13:20:43 +0530

    Datatypes

---
+++ 03.js
@@ -0,0 +1,34 @@
+//Datatypes and ECMA
+
+"use strict";//  treats all JS code as newer version
+
+//alert(3+3) the syntax is invalid for IDE but if you are using browser in console mode then it will work
+
+//code readability is having the highest priority
+
+//ECMA is for standard for JS and MDN can also be used as reference
+
+let name="Optimus Max"
+let age=11
+let loss=false
+
+/*Datatypes:
+number 2^53
+bigint
+string=> ""
+bool = true/false
+null=>standalone value
+undefined
+
+The difference between the null and undefined is that in undefined the value is not assigned whereas in null the value of the varible is null
+
+symbol(mostly used in reactjs) is used for define uniqueness
+
+objects
+
+typeof is a method which is used for know the datatype of the varible
+
+Interserting thing is that the typeof null is object it is said that this the error of the lang; typeof undefined is undefined
+
+*/
+
